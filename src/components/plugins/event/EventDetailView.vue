9a41f4572ce0915e22494f8fa82eaaab682a99ce
addiinnocent <yannik@Homematic-CCU2.fritz.box>
2024-09-23 18:24:58 +0200

    Event list, event view

---
+++ src/components/plugins/event/EventDetailView.vue
@@ -0,0 +1,93 @@
+<template>
+  <div className="min-h-screen flex justify-center items-center">
+    <Card className="pb-10">
+      <div className="mb-2 flex flex-row justify-between">
+        <BackButton />
+      </div>
+
+      <div class="flex">
+        <div class="w-16 h-16 flex-shrink-0 flex flex-col justify-center items-center border-2 rounded-md mr-4">
+          <span class="text-2xl font-bold">{{ eventDay }}</span>
+          <span class="text-sm font-medium">{{ eventMonth }}</span>
+        </div>
+
+        <div class="flex-1">
+          <h1 class="text-4xl font-bold text-gray-900 mb-4">
+            {{ event?.title }}
+          </h1>
+
+          <p class="text-gray-700 mb-6">
+            {{ event?.description }}
+          </p>
+
+          <div class="mb-8">
+            <h2 class="text-lg font-semibold text-gray-900 mb-2">
+              Relevant interests:
+            </h2>
+            <div class="flex flex-wrap gap-2">
+              <InterestBadge
+                v-for="interest in event?.interests"
+                :key="interest.id"
+                :title="interest.title"
+              />
+            </div>
+          </div>
+
+          <div class="mb-8">
+            <h2 class="text-lg font-semibold text-gray-900 mb-2">
+              People who already joined:
+            </h2>
+            <div class="flex flex-wrap gap-2">
+              <ProfileImage
+                v-for="profile of profiles"
+                size="medium"
+                :src="profile?.image"
+                :tooltipText="profile?.username"
+              />
+            </div>
+          </div>
+
+          <div class="mt-6">
+            <button class="px-6 py-2 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700 transition duration-150">
+              Join the Event
+            </button>
+          </div>
+        </div>
+      </div>
+    </Card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import events from './service.ts';
+import { ref, inject, computed, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import Card from '@/components/common/CardComponent.vue';
+import Title from '@/components/common/TitleComponent.vue';
+import BackButton from '@/components/common/BackButton.vue';
+import InterestBadge from '@/components/badges/InterestBadge.vue';
+import ProfileImage from '@/components/common/ProfileImage.vue';
+
+const route = useRoute();
+const event = ref(null);
+const profile = inject('profile');
+const eventDate = computed(() => new Date(event.value?.date));
+const eventDay = computed(() => eventDate.value?.getDate());
+const eventMonth = computed(() => eventDate.value?.toLocaleString('default', { month: 'short' }));
+
+const profiles = computed(() => [profile.value, profile.value, profile.value])
+
+const fetchEvent = async (id: string) => {
+  try {
+    const response = events.find(x => x.id == id);
+
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+onMounted(async () => {
+  event.value = await fetchEvent(route.params.id);
+});
+</script>
--- src/components/plugins/event/Index.vue
+++ src/components/plugins/event/Index.vue
@@ -1,21 +1,11 @@
 <template>
   <div v-for="event of events">
-    <Dialog>
-      <template #trigger="{ openDialog }">
-        <EventListItem
-          :key="event.id"
-          :event="event"
-          @click.stop="openDialog"
-        />
-      </template>
-
-      <template #content="{ closeDialog }">
-        <EventDetailDialog
-          :event="event"
-          :closeDialog="closeDialog"
-        />
-      </template>
-    </Dialog>
+    <router-link :to="`/events/${event.id}`">
+      <EventListItem
+        :key="event.id"
+        :event="event"
+      />
+    </router-link>
   </div>
 </template>
 
@@ -24,44 +14,10 @@ import { ref } from 'vue';
 import Dialog from '../../../components/dialog/DialogComponent.vue';
 import EventListItem from './EventListItem.vue';
 import EventDetailDialog from './EventDetailDialog.vue';
+import events from './service.ts';
 
 const props = defineProps({
 
 });
-
-const events = [
-  {
-    id: 1,
-    title: 'Beach cleaning',
-    date: new Date(),
-    description: 'We will be cleaning the beach in Ponta do sol',
-    interests: [
-      { id: 1, title: 'Nature' },
-      { id: 2, title: 'Beach' },
-      { id: 3, title: 'Volunteers' }
-    ]
-  },
-  {
-    id: 2,
-    title: 'Dog sitting',
-    date: new Date(),
-    description: 'Looking for a dog sitter',
-    interests: [
-      { id: 1, title: 'House' },
-      { id: 2, title: 'Animals' }
-    ]
-  },
-  {
-    id: 3,
-    title: 'Tree planting',
-    date: new Date(),
-    description: 'Gathering people for a tree planting',
-    interests: [
-      { id: 1, title: 'Nature' },
-      { id: 2, title: 'Tree' },
-      { id: 3, title: 'Forest' }
-    ]
-  },
-]
 
 </script>
