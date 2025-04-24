49b0a99329395e46f86abd0f4cd952c7d63eabca
Felix Böhm <kontakt@felixboehm.it>
2025-04-24 11:38:05 +0700

    comments on client auth

--- client/src/composables/userProvider.ts
+++ client/src/composables/userProvider.ts
@@ -10,6 +10,9 @@ export function userProvider() {
 
   const register = async (formData: FormData) => {
     return new Promise(async (resolve, reject) => {
+      // TODO: should be email. Suggest to hash(email) and use the hash to store the data.
+      // gun.get('credentials').get(emailHash).put(...);
+
       const username = formData.get('username');
       const challenge = crypto.getRandomValues(new Uint8Array(32));
 
@@ -28,9 +31,14 @@ export function userProvider() {
       };
 
       const cred = await navigator.credentials.create({ publicKey });
+      console.log('Registration credential:', cred);
+
+      // Should we use the email hash as the derived username?
       const rawId = cred.rawId;
       const usernameDerived = bufferEncode(cred.rawId);
-
+      // I assume this isn't secure. Needs sea.work(id,salt)
+      // Then it can't be brute forced or rainbow table attacked.
+      // Why not use cred.id? It is a string already
       const hashBuffer = await crypto.subtle.digest('SHA-256', rawId);
       const passwordDerived = bufferEncode(hashBuffer);
       console.log('rawid', rawId);
@@ -38,6 +46,9 @@ export function userProvider() {
 
       gun.get('credentials').get(username).put({
         id: bufferEncode(rawId),
+        // Q: This is basically empty. Why?
+        // The attestationObject is containing import infos: 
+        // credentialID, publicKey, counter, credentialPublicKey, ...
         credential: JSON.stringify({
           clientDataJSON: bufferEncode(cred.clientDataJSON),
           attestationObject: bufferEncode(cred.attestationObject),
@@ -48,6 +59,7 @@ export function userProvider() {
         if (ack.err) {
           reject('Create failed:', ack.err);
         } else {
+          // Q: No there isa key pair created - and we need to store it encrypted??
           resolve(ack);
         }
       });
@@ -72,7 +84,12 @@ export function userProvider() {
         };
 
         const cred = await navigator.credentials.get({ publicKey });
-        const rawId = cred.rawId;
+        console.log('Login credential:', publicKey, cred);
+        // cred returns a cred.response.signature which needs to be verified.
+        // verify(pubKey, signature, challenge) needs to ensure the signature is valid.
+        // https://github.com/mylofi/webauthn-local-client?tab=readme-ov-file#verifying-an-authentication-response
+        // https://github.com/mylofi/webauthn-local-client/blob/main/src/walc.js#L419
+        const rawId = cred.rawId;  // Why not use id? It is a string already
         const usernameDerived = bufferEncode(rawId);
 
         const hashBuffer = await crypto.subtle.digest('SHA-256', rawId);
@@ -90,6 +107,7 @@ export function userProvider() {
   }
 
   const logout = async () => {
+    // gun.user().leave(); ??
     localStorage.removeItem('user');
     user.value = null;
   }
@@ -99,6 +117,8 @@ export function userProvider() {
   });
 
   onMounted(() => {
+    // The recall function attempts to restore the user's authentication session 
+    // without requiring them to log in again. It checks for stored session data
     gun.user().recall({ sessionStorage: true });
     if (gun.user().is) {
       gun.user()
