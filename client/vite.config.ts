7e858c44f06755f6a8a2baae186fe98b8c0901cb
addiinnocent <johnysmoke@web.de>
2025-05-19 13:42:35 +0200

    Normalized vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,47 +5,6 @@ import tailwindcss from "@tailwindcss/vite"
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import federation from '@originjs/vite-plugin-federation'
-import gun from './src/services/gun'
-
-const url = 'http://localhost';
-
-/*async function fetchPluginList() {
-  return new Promise((resolve) => {
-    gun.get('plugins').on((data) => {
-      resolve(data || {});
-    });
-  });
-}*/
-
-async function fetchPluginList() {
-  try {
-    const response = await axios.get(`${url}:3000/api/v2/plugins`);
-    console.log(response.data)
-    
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching plugin list:', error);
-    throw error;
-  }
-}
-
-
-async function createFederationRemotes() {
-  const pluginList = await fetchPluginList() || [];
-
-  const remotes = {};
-  Object.entries(pluginList).forEach(([pluginName, pluginUrl]) => {
-    remotes[pluginName] = `${url}:3002/assets/plugin.js`;
-  });
-
-  //return remotes;
-
-  return {
-    chat_plugin: `${url}:3001/assets/plugin.js`,
-    //wiki_plugin: `${url}:3002/assets/plugin.js`,
-    event_plugin: `${url}:3003/assets/plugin.js`,
-  };
-}
 
 export default defineConfig({
   plugins: [
@@ -67,9 +26,4 @@ export default defineConfig({
     target: 'esnext',
     outDir: './server/src/views'
   },
-  /*server: {
-    watch: {
-      ignored: ['**']
-    }
-  }*/
 });
