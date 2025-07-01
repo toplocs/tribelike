2ff3cdde49c022f91889d9dc81d7439fc547dd06
addiinnocent <johnysmoke@web.de>
2025-07-01 16:38:00 +0200

    ra-data to gitignore

--- client/src/assets/relationKeys.ts
+++ client/src/assets/relationKeys.ts
@@ -1,108 +1,109 @@
-export const profileToTopic = [
+export const profileToTopic = [];
+export const profileToLocation = [];
+export const topicToLocation = [];
+export const topicToTopic = [];
+
+export const profileRelations = [
   {
     id: 'like',
     active: 'liking',
     passive: 'liked by',
     color: 'green',
     icon: 'heart',
+    accepts: ['topic'],
   }, {
     id: 'love',
     active: 'loving',
     passive: 'loved by',
     color: 'red',
     icon: 'heart',
+    accepts: ['topic'],
   }, {
     id: 'learn',
     active: 'learning',
     passive: 'learned by',
     color: 'blue',
     icon: 'study',
+    accepts: ['topic'],
   }, {
     id: 'teach',
     active: 'teaching',
     passive: 'teached by',
     color: 'yellow',
     icon: 'study',
-  }, 
-];
-
-export const profileToLocation = [
-  {
+    accepts: ['topic'],
+  }, {
     id: 'visit',
     active: 'visiting',
     passive: 'visited by',
     color: 'green',
     icon: 'heart',
+    accepts: ['location'],
   }, {
     id: 'live',
     active: 'living in',
     passive: 'living here',
     color: 'red',
     icon: 'house',
+    accepts: ['location'],
   }, {
     id: 'going',
     active: 'going to',
     passive: 'the destination of',
     color: 'blue',
     icon: 'shoe',
+    accepts: ['location'],
   }, {
     id: 'work',
     active: 'working in',
     passive: 'workingplace of',
     color: 'yellow',
     icon: 'study',
+    accepts: ['location'],
   }, 
 ];
 
-export const topicToTopic = [
+export const topicRelations = [
   {
     id: 'child',
     active: 'child of',
     passive: 'parent of',
     color: 'green',
     icon: '',
+    accepts: ['topic'],
   }, {
     id: 'category',
-    active: 'a',
+    active: 'category of',
     passive: 'contains',
     color: 'blue',
     icon: '',
+    accepts: ['topic'],
   },
 ];
 
-export const topicToLocation = [
-  {
-    id: 'in',
-    active: 'in',
-    passive: 'contains',
-    color: 'blue',
-    icon: '',
-  }
-];
 
-export const locationToLocation = [
+export const locationRelations = [
   {
     id: 'child',
     active: 'child of',
     passive: 'parent of',
     color: 'green',
     icon: '',
+    accepts: ['location'],
   }, {
     id: 'category',
-    active: 'a',
+    active: 'category of',
     passive: 'contains',
     color: 'blue',
     icon: '',
+    accepts: ['topic'],
   },
 ];
 
-export const relationTypes = [
-  ...profileToTopic,
-  ...profileToLocation,
-  ...topicToLocation,
-  ...topicToTopic,
-  ...locationToLocation,
+export const relationKeys = [
+  ...profileRelations,
+  ...topicRelations,
+  ...locationRelations,
 ];
 
-export const relationTypeIds = Array.from(new Set(relationTypes.map(r => r.id)));
-
+export const relationKeyIds = Array.from(new Set(relationKeys.map(r => r.id)));
